acdea7d0a8dbdbda98a29ffcb208666a3c8360df
sofianbello <safian.bello@haw-hamburg.de>
2021-12-16 20:00:11 +0100

    Three-Reactive-Images

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'lil-gui'
+import gsap from 'gsap'
+import { MOUSE } from 'three'
+
+// Debug
+const gui = new dat.GUI()
 
 /** 
  * LOADERS
@@ -8,13 +14,12 @@ import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
  const textureLoader = new THREE.TextureLoader()
 
+
  // TEXTURES
- 
- const object2Texure = textureLoader.load('https://raw.githubusercontent.com/sofianbello/ThreeFolio/main/img/sdlfsdfgklj.jpg')
- const matcapTexture1 = textureLoader.load('https://raw.githubusercontent.com/nidorx/matcaps/master/1024/736655_D9D8D5_2F281F_B1AEAB.png')
- const matcapTexture2 = textureLoader.load('https://raw.githubusercontent.com/nidorx/matcaps/master/1024/3A2412_A78B5F_705434_836C47.png')
- const imgTexture = new THREE.TextureLoader().load('https://images.unsplash.com/photo-1638932030844-b584074bd619?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2022&q=80')
- 
+//  const map1 = new textureLoader.load(`/img/Examples/0.jpg`)
+//  const map2 = new textureLoader.load(`/img/Examples/1.jpg`)
+//  const map3 = new textureLoader.load(`/img/Examples/2.jpg`)
+//  const map4 = new textureLoader.load(`/img/Examples/3.jpg`)
  
 
 // EVENTS
@@ -29,33 +34,49 @@ window.addEventListener('resize', () =>
   // Update Renderer
   renderer.setSize(sizes.width,sizes.height)
 })
+// CANVAS
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
 const scene = new THREE.Scene();
-const canvas = document.querySelector('canvas.webgl');
-
 
 // Object#1
 
-const geo1 = new THREE.BoxBufferGeometry( 1,1,1, 5,5,5)
-const mat1 = new THREE.MeshMatcapMaterial({matcap: matcapTexture2, wireframe: false})
-const obj1 = new THREE.Mesh(geo1,mat1)
-scene.add(obj1)
+const geo = new THREE.PlaneBufferGeometry( 1, 1.3)
+
+
+for(let i = 0; i< 4; i++ ) {
+  const mat = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(`/img/Examples/${i}.jpg`)
+  })
+  const img = new THREE.Mesh(geo,mat)
+  img.position.set(Math.random()+.3, i*-1.6) 
+  scene.add(img)
+}
+
+let objs = []
+
+scene.traverse((object) => {
+  if (object.isMesh)
+      objs.push(object)
+})
 
 // Camera
 
-const sizes = {
+const sizes = {  
   width: window.innerWidth,
   height: window.innerHeight 
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-camera.position.x = 1.5;
-camera.rotation.y = 0.5;
+camera.position.z = 2;
+camera.position.x = 0;
+camera.rotation.y = 0;
 scene.add(camera);
 
-const controls = new OrbitControls(camera,canvas)
+// const controls = new OrbitControls(camera,canvas)
+
+gui.add(camera.position,'y').min(-5).max(5).step(0.1)
 
 /**
  * LIGHTS
@@ -76,19 +97,72 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-// renderer.render(scene, camera)
+
+// MOUSE
+
+
+let y = 0
+let position = 0
+
+const onMouseWheel = (event) =>{
+  y = event.deltaY * 0.0007
+}
+
+window.addEventListener("wheel", onMouseWheel)
+
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = -(event.clientY / sizes.height) * 2 + 1
+})
+
+
+
+
 
 /**
  * Animate
  */
+
+// RayCaster
+const rayCaster = new THREE.Raycaster()
+
  const clock = new THREE.Clock()
 
  const tick = () => 
  {
    const elapsedTime = clock.getElapsedTime()
+   const deltaTime =
  
- 
-   controls.update()
+  // OnScrollAnimation
+    position += y
+    camera.position.y = position
+    y *= .94
+
+    camera.position.y = -position
+    
+    // RayCaster
+
+    rayCaster.setFromCamera(mouse, camera)
+    const intersects = rayCaster.intersectObjects(objs)
+    
+    for(const intersect of intersects) {
+      gsap.to(intersect.object.scale, {x: 1.5,y:1.5});
+      gsap.to(intersect.object.rotation, {y:-0.5});
+      gsap.to(intersect.object.position, {z:-0.9});
+    }
+
+    for (const object of objs) {
+      if(!intersects.find(intersect => intersect.object === object)){
+        gsap.to(object.scale, {x:1,y:1});
+        gsap.to(object.rotation, {y:0});
+        gsap.to(object.position, {z:0});
+      }
+    }
+
+
+  //  controls.update()
    // Render
    renderer.render(scene,camera)
  
